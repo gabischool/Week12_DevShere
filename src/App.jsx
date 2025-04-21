e864bb4433cb4adb601a72e7eecf2af632d64943
farahgarow <frxyare123@gmail.com>
2025-04-21 17:45:50 +0300

    Finshing Assignment week12

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
 
 // TODO: Import react router dom here
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Footer from './components/Footer';
 import './styles/App.css';
@@ -13,6 +16,10 @@ function App() {
         <Navbar />
         <main className="main-content">
          {/* TODO: Add react router dom routes here */}
+         <Routes>
+          <Route path='/'element ={<Home />}/>
+          <Route path='/projects'element ={<Projects />}/>
+         </Routes>
          
         </main>
         <Footer />
