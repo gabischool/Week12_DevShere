9ec0342c3ecbb6e700f0df29f5fd9c65fcc743cc
GureyAYGARAD <ahmed-nur@msn.com>
2025-04-28 23:22:09 +0200

    Week12donealx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Home from './pages/Home'
+import Projects from './pages/Projects';
 
 // TODO: Import react router dom here
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 
 import Footer from './components/Footer';
 import './styles/App.css';
 
+import { PrinterIcon } from 'lucide-react';
+
 function App() {
+ 
+  
   return (
     <Router>
       <div className="app">
         <Navbar />
         <main className="main-content">
-         {/* TODO: Add react router dom routes here */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
          
+         
+         {/* TODO: Add react router dom routes here */}
+          </Routes>
         </main>
         <Footer />
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 // TODO: Import LINK from react router dom components
+import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
 
 import { Github } from 'lucide-react';
@@ -19,7 +20,13 @@ const Navbar = () => {
 
         <ul className="navbar-menu">
           <li className="navbar-item">
-           {/* TODO: Add the home route here */}
+           {/* TODO: Add the home route here */
+            <> 
+             <Link to="/">Home </Link>
+             <Link to="/projects">Projects </Link>
+            </>
+           }
+
           </li>
           <li className="navbar-item">
             {/* TODO: Add the projects route here */}
